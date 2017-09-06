8256d6c887c20d5a538ad5b3d2c3fed7a38b7602
Onur Tanrıkulu <onurtnrkl@gmail.com>
2017-09-06 23:10:25 +0300

    Scripts initialized.
    Dialog functions added.
    Notification functions added.
    UI improvements.

---
+++ Assets/Alertify/Dialog.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Alertify
+{
+    public sealed class Dialog : MonoBehaviour
+	{
+        private static Dialog instance;
+
+        private void Awake()
+        {
+            if(instance != null && instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                instance = this;
+            }
+
+			//FIXME: DontDestroyOnLoad only work for root GameObjects or components on root GameObjects.
+			DontDestroyOnLoad(gameObject);
+        }
+
+        public static void Alert(string dialog, UnityAction ok)
+        {
+            
+        }
+
+        public static void Confirm(string dialog, UnityAction ok, UnityAction cancel)
+        {
+            
+        }
+
+        public static void Prompt(string dialog, UnityAction ok, UnityAction cancel)
+        {
+            
+        }
+    }   
+}
